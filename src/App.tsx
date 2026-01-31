439149faaea9c070947b26649a092a60b5c0f79f
Sulthansyed <sulthanibrahim90@gmail.com>
2026-02-01 00:49:26 +0800

    Replace BrowserRouter with HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// CHANGE: Import HashRouter instead of BrowserRouter
+import { HashRouter, Routes, Route } from "react-router-dom"; 
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Units from "./pages/Units";
@@ -19,7 +20,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* CHANGE: Use HashRouter here */}
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
@@ -30,7 +32,7 @@ const App = () => (
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
